fa66ca963c157649f5d036b8df925c4cd4161863
sadiq-b <babalolasadiq@yahoo.com>
2020-08-14 12:50:24 +0100

    final version

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const navtoggle= document.querySelector('.nav-toggle')
+const navLinks=document.querySelectorAll('.nav__link')
+navtoggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach  (link =>
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+)
